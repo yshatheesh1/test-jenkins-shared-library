4a8efc2a0a579cb1db51ed738b068d903b8434d9
= <yshatheesh@gmail.com>
2021-10-20 22:55:40 -0500

    test

--- vars/codePipeline.groovy
+++ vars/codePipeline.groovy
@@ -3,23 +3,26 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    stage('test') {
-        switch (pipelineParams.buildType.toLowerCase()) {
-            case 'flutter':
-                echo 'flutter'
-                break
-            case 'dotnetcore' :
-                echo 'dotnetcore'
-                break
-            case 'java':
-                echo 'java'
-                break
-            case 'angular':
-                echo 'angular'
-                break
-            case 'infrastructure':
-                echo 'infrastructure'
-                break
+    node {
+        stage('test') {
+            switch (pipelineParams.buildType.toLowerCase()) {
+                case 'flutter':
+                    echo 'flutter'
+                    break
+                case 'dotnetcore' :
+                    echo 'dotnetcore'
+                    break
+                case 'java':
+                    echo 'java'
+                    break
+                case 'angular':
+                    echo 'angular'
+                    break
+                case 'infrastructure':
+                    echo 'infrastructure'
+                    break
+            }
         }
     }
+
 }
