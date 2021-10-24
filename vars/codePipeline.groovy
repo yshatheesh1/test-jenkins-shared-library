f19c51c23a5aa7ad206bcb8dc12b81c00ce2451e
= <yshatheesh@gmail.com>
2021-10-24 00:20:59 -0500

    initialized gradle wrapper

--- vars/codePipeline.groovy
+++ vars/codePipeline.groovy
@@ -27,11 +27,11 @@ def call(body) {
     stepExecutor.echo(config.credentialId as String)
     node {
         stage('checkout') {
-            stepExecutor.echo(scm.credentialId as String)
+            stepExecutor.echo(scm.credentialsId as String)
             stepExecutor.echo(scm.branches as String)
             stepExecutor.echo(scm.userRemoteConfigs[0] as String)
             Checkout checkoutHandler = new Checkout(stepExecutor);
-            checkoutHandler.gitCheckout("GitSCM", scm.userRemoteConfigs[0].url, scm.credentialId, scm.branches);
+            checkoutHandler.gitCheckout("GitSCM", scm.userRemoteConfigs[0].url, scm.credentialsId, scm.branches);
         }
     }
 }
