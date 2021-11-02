72c24472e44f8a18a6eeba1537373aaf170762c2
= <yshatheesh@gmail.com>
2021-11-02 00:59:47 -0500

    initialized gradle wrapper

--- vars/codePipeline.groovy
+++ vars/codePipeline.groovy
@@ -28,11 +28,11 @@ def call(body) {
     stepExecutor.echo(scm.userRemoteConfigs[0].credentialsId)
     node('master') {
         stage('checkout') {
-            gitCheckout([
-                    type: 'GitSCM',
-                    url: scm.userRemoteConfigs[0].url,
-                    credentialId: scm.userRemoteConfigs[0].credentialsId,
-                    branchName: scm.branches[0].name]
+            gitCheckout(
+//                    type: 'GitSCM',
+//                    url: scm.userRemoteConfigs[0].url,
+//                    credentialId: scm.userRemoteConfigs[0].credentialsId,
+//                    branchName: scm.branches[0].name
             )
         }
         docker.image(config.image).inside {
